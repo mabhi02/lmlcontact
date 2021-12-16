62117f6e8a7edcd1b7e16435af429d3c7779e8ac
mabhi02 <mabhi02@yahoo.com>
2021-12-16 09:59:31 -0800

    f
    f

--- config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url = parse_url(getenv("mysql://b779551da75fbb:e21ad95b@us-cdbr-east-05.cleardb.net/heroku_bf57ae6961c936f?reconnect=true"));
-
-$server = $url["us-cdbr-east-05.cleardb.net"];
-$username = $url["b779551da75fbb"];
-$password = $url["e21ad95b"];
-$db = substr($url["connectDB.php"], 1);
-$conn = new mysqli($server, $username, $password, $db);
-?>
--- connectDB.php
+++ connectDB.php
@@ -1,4 +1,16 @@
 <?php
+
+<?php
+$url = parse_url(getenv("mysql://b779551da75fbb:e21ad95b@us-cdbr-east-05.cleardb.net/heroku_bf57ae6961c936f?reconnect=true"));
+
+$server = $url["us-cdbr-east-05.cleardb.net"];
+$username = $url["b779551da75fbb"];
+$password = $url["e21ad95b"];
+$db = substr($url["connectDB.php"], 1);
+$conn = new mysqli($server, $username, $password, $db);
+?>
+
+<?php
 $name="heroku_bf57ae6961c936f";
 $username = "b779551da75fbb";
 $password = "e21ad95b";
