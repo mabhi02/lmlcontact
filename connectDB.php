28dce9dce323140f96e782daa71b4910fcc992e2
Abhijith Varma Mudunuri <66578240+mabhi02@users.noreply.github.com>
2021-12-15 22:48:05 -0800

    Add files via upload

---
+++ connectDB.php
@@ -0,0 +1,29 @@
+<?php
+$name="localhost";
+$username = "root";
+$password = "";
+$database = "lml";
+$connect=mysqli_connect($name,$username,$password,$database);
+if(!$connect)
+{
+  die("Could not connect" . mysqli_connect_error());
+}
+if(isset($_POST['sub']))
+{
+  $username1 = $_POST['username'];
+  $message = $_POST['message'];
+  $email = $_POST['email'];
+  $cellphone = $_POST['cellphone'];
+  $sql_query = "INSERT INTO lmldata (username1,message,email,cellphone)
+  VALUES ('$username1','$message','$email','$cellphone')";
+  if(mysqli_query($connect, $sql_query))
+  {
+    echo "Connected";
+  }
+  else
+  {
+    echo "Error: " . $sql . "" . mysqli_error($connect);
+  }
+  mysqli_close($connect);
+}
+?>
