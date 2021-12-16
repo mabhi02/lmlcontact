d67769943c2c406b1b77a03f5785bbb04b57ad5d
mabhi02 <mabhi02@yahoo.com>
2021-12-16 09:58:27 -0800

    f
    f

--- config.inc.php
+++ config.inc.php
@@ -1,81 +1,9 @@
 <?php
-/*
- * This is needed for cookie based authentication to encrypt password in
- * cookie
- */
-$cfg['blowfish_secret'] = 'xampp'; /* YOU SHOULD CHANGE THIS FOR A MORE SECURE COOKIE AUTH! */
+$url = parse_url(getenv("mysql://b779551da75fbb:e21ad95b@us-cdbr-east-05.cleardb.net/heroku_bf57ae6961c936f?reconnect=true"));
 
-/*
- * Servers configuration
- */
-$i = 0;
-
-/*
- * First server
- */
-$i++;
-
-/* Authentication type and info */
-$cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'root';
-$cfg['Servers'][$i]['password'] = '';
-$cfg['Servers'][$i]['extension'] = 'mysqli';
-$cfg['Servers'][$i]['AllowNoPassword'] = true;
-$cfg['Lang'] = '';
-
-/* Bind to the localhost ipv4 address and tcp */
-$cfg['Servers'][$i]['host'] = '127.0.0.1';
-$cfg['Servers'][$i]['connect_type'] = 'tcp';
-
-/* User for advanced features */
-$cfg['Servers'][$i]['controluser'] = 'pma';
-$cfg['Servers'][$i]['controlpass'] = '';
-
-/* Advanced phpMyAdmin features */
-$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
-$cfg['Servers'][$i]['relation'] = 'pma__relation';
-$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
-$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
-$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
-$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
-$cfg['Servers'][$i]['history'] = 'pma__history';
-$cfg['Servers'][$i]['designer_coords'] = 'pma__designer_coords';
-$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
-$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
-$cfg['Servers'][$i]['recent'] = 'pma__recent';
-$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
-$cfg['Servers'][$i]['users'] = 'pma__users';
-$cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
-$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
-$cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
-$cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
-$cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
-$cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
-$cfg['Servers'][$i]['favorite'] = 'pma__favorite';
-
-/* Heroku remote server */
-$i++;
-$cfg["Servers"][$i]["host"] = "us-cdbr-east-05.cleardb.net"; //provide hostname
-$cfg["Servers"][$i]["user"] = "b779551da75fbb"; //user name for your remote server
-$cfg["Servers"][$i]["password"] = "e21ad95b"; //password
-$cfg["Servers"][$i]["auth_type"] = "config"; // keep it as config
-
-
-
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("mysql://b779551da75fbb:e21ad95b@us-cdbr-east-05.cleardb.net/heroku_bf57ae6961c936f?reconnect=true"));
-$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
-$cleardb_username = $cleardb_url["b779551da75fbb"];
-$cleardb_password = $cleardb_url["e21ad95b"];
-$cleardb_db = substr($cleardb_url["mysql://b779551da75fbb:e21ad95b@us-cdbr-east-05.cleardb.net/heroku_bf57ae6961c936f?reconnect=true"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-/*
- * End of servers configuration
- */
-
+$server = $url["us-cdbr-east-05.cleardb.net"];
+$username = $url["b779551da75fbb"];
+$password = $url["e21ad95b"];
+$db = substr($url["connectDB.php"], 1);
+$conn = new mysqli($server, $username, $password, $db);
 ?>
