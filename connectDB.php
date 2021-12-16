ed162f8b3ab01c8d9268e3180573ffbb1cc1f1b3
mabhi02 <mabhi02@yahoo.com>
2021-12-16 13:48:30 -0800

    f
    f

--- connectDB.php
+++ connectDB.php
@@ -6,10 +6,10 @@ $server = $url["us-cdbr-east-05.cleardb.net"];
 $username = $url["b779551da75fbb"];
 $password = $url["e21ad95b"];
 $db = substr($url["heroku_bf57ae6961c936f"], 1);
-$conn = mysqli_connect($server, $username, $password, $db);
-if(!$conn)
+$connect=mysqli_connect($server,$username,$password,$db);
+if(!$connect)
 {
-  die("Could not connnect" . mysqli_connect_error());
+  die("Could not connect" . mysqli_connect_error());
 }
 if(isset($_POST['sub']))
 {
@@ -19,14 +19,14 @@ if(isset($_POST['sub']))
   $cellphone = $_POST['cellphone'];
   $sql_query = "INSERT INTO lmldata (username1,message,email,cellphone)
   VALUES ('$username1','$message','$email','$cellphone')";
-  if(mysqli_query($conn, $sql_query))
+  if(mysqli_query($connect, $sql_query))
   {
     echo "Connected";
   }
   else
   {
-    echo "Error: " . $sql . "" . mysqli_error($conn);
+    echo "Error: " . $sql . "" . mysqli_error($connect);
   }
-  mysqli_close($conn);
+  mysqli_close($connect);
 }
 ?>
